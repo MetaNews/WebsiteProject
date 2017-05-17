eed326c10694a7f15fe6e600985beca2e7e6247c
James Skipp <jamesskipp2@gmail.com>
2017-05-17 17:55:32 -0400

    Update and rename navigation.js to lrc.js

--- static/js/lrc.js
+++ static/js/lrc.js
@@ -1,24 +1,58 @@
-
 $(document).ready(function(){
+    resizer($('#top-nav').width(), 962);
+    
+	$(window).on('resize', function(){
+	    var win = $(this); //this = window
+	    screenLen = $('#top-nav').width();
+	    elemLen = 962;
+	    resizer(screenLen, elemLen);
+	});
+	
+	// Button Clicks
+	
     $(".nav-tabs a").click(function(){
         $(this).tab('show');
     });
-    
-    resizer($('#top-nav').width(), 962);
-    
+        
     $("#quality-btns button").click(function(){
-    	$(".btn-group button").removeClass("active");
-    	$(this).addClass("active");
+    	if (!($(this).hasClass("active"))) {
+        	$(".btn-group button").removeClass("active");
+        	$(this).addClass("active");
+    	}
+    	else {
+    		$(this).removeClass("active");
+    	}
     });
+    
+	$('#btnSignUp').click(function(){
+		if (($('#inputEmail').val().length == 0) || ($('#inputEmail').val().length == 0) || ($('#inputPassword').val().length == 0)) {
+			$('#nul-warning').text("You can't leave any fields blank.");
+		}
+		else if ($('#inputPassword').val().length < 6) { //Work around for InvalidParameterException with passwords < 6 characters.
+			$('#nul-warning').text("Passwords must be at least 8 characters in length.");
+		}
+		else {
+			register($('#inputEmail').val(), $('#inputUsername').val(), $('#inputPassword').val());
+			clearform('nul-form');
+		}
+	});
+	
+	$('#nul-close').click(function () {
+		clearform('nul-form');
+		$('#nul-warning').text('');
+	});
+	
 });
 
-$(window).on('resize', function(){
-    var win = $(this); //this = window
-    screenLen = $('#top-nav').width();
-    // elemLen = 151 + $('#quality-tabs').width() + $('#ideo-tabs').width() + $('#advanced-search').width();
-    elemLen = 962;
-    resizer(screenLen, elemLen);
-});
+function clearform(element) {
+	var elements = document.getElementById(element)
+	
+	for (var i = 0; i < elements.length; i++) {
+		if (elements[i].className == "form-control") {
+			elements[i].value = "";
+		}
+	}	
+}
 
 function resizer(screenLen, elemLen) {
     if (screenLen < 768 )
